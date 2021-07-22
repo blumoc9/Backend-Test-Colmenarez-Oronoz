44db530dd48483ff7ccdf35316976e1b3fceffd0
Fernando Colmenarez Oronoz <fcolmenarez@technisys.com>
2021-07-22 18:50:58 -0400

    menu app added

---
+++ cornershop-backend-test/menu/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+
