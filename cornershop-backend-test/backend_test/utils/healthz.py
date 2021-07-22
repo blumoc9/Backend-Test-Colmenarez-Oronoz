7eec19dd94bd784cf697abcb106eea61515a0a9c
Fernando Colmenarez Oronoz <fcolmenarez@technisys.com>
2021-07-22 17:44:19 -0400

    solve problem

---
+++ cornershop-backend-test/backend_test/utils/healthz.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.response import Response
+
+
+@api_view(["GET", "HEAD"])
+@permission_classes([permissions.AllowAny])
+@authentication_classes([])
+def healthz(request, *args, **kwargs):
+    content = {
+        'user': "Fernando",
+        'auth': "Nada"
+    }
+    return Response(content)
